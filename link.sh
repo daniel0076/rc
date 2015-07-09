e0f3bde26aa23b64c3eea1d9d05ec5e305adcae9
Daniel Tsai <idaniel.twc@gmail.com>
2015-07-10 00:04:46 +0800

    add submodule init script

--- link.sh
+++ link.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-files=`env ls|grep -v "\."`
-dir=`env pwd`
-for f in $files;do
-    if [ -L $HOME/.${f} ];then
-        rm $HOME/.${f}
-    fi
-    env ln -s $dir/$f $HOME/.${f}
-    if [ $? -eq 0 ];then
-        echo ".$f linked"
-    fi
-done
+#files=`env ls|grep -v "\."`
+#dir=`env pwd`
+#for f in $files;do
+    #if [ -L $HOME/.${f} ];then
+        #rm $HOME/.${f}
+    #fi
+    #env ln -s $dir/$f $HOME/.${f}
+    #if [ $? -eq 0 ];then
+        #echo ".$f linked"
+    #fi
+#done
+
+echo "initiating submodules"
+cd "./zsh/zsh-git-prompt"
+git submodule init
+git submodule update
